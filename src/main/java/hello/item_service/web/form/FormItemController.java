0148fb3db88bea7475c26e493dc290e547e1b0ad
minkyung kim <minry20415@naver.com>
2024-12-21 13:47:26 +0900

    ✨: Thymeleaf 체크박스&멀티체크박스
    - 타임리프에서는 th:field="${item.open}"으로 서버에 true/false 값을 넘겨준다.
    - 개발자가 '만약, true이면 checked 속성값을 넣고false이면 checked를 제외한다' 코드를 직접 작성할 필요 없이 타임리프에서 지원해준다.
    - 멀티 체크박스는 th:each="...: ${...}" 반복을 통해 체크박스를 여러개 뿌려주고, id 속성값은 th:for="${#ids.prev('...')}로 생성한다.

--- src/main/java/hello/item_service/web/form/FormItemController.java
+++ src/main/java/hello/item_service/web/form/FormItemController.java
@@ -10,7 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +21,16 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String, String> region() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
 
     // 목록 조회
     @GetMapping
@@ -33,6 +45,7 @@ public class FormItemController {
     public String item(Model model, @PathVariable("itemId") Long itemId) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "form/item";
     }
 
@@ -40,6 +53,7 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
@@ -90,6 +104,8 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -101,6 +117,7 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item findItem = itemRepository.findById(itemId);
         model.addAttribute("item", findItem);
+
         return "form/editForm";
 
     }
