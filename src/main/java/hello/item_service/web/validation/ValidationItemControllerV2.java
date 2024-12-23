cce7e1a2d8cbe2a885cccdfbe7bdb5760f3c3007
minkyung kim <minry20415@naver.com>
2024-12-23 16:21:40 +0900

    ✨: BindingResult을 사용하여 리팩터링
    - 스프링에서 제공하는 BindingResult을 사용하면 별도의 Map을 만들어 오류들을 보관할 필요없이 BindingResult에 FieldError 객체를 만들어 보관하면됨
    - 글로벌 에러는 ObjectError 객체 사용
    - 타임리프에서는 #fields를 이용하여 사용

--- src/main/java/hello/item_service/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/item_service/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -74,81 +77,40 @@ public class ValidationItemControllerV2 {
     }
 
     // 등록
-//    @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam Integer quantity,
-                       Model model) {
-        Item item = new Item(itemName, price, quantity);
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-
-        return "validation/v2/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item) {
-        itemRepository.save(item);
-
-//        model.addAttribute("item", item);
-
-        return "validation/v2/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) {
-        // name="" 생략 시 Item -> item 로 변경하여 model에 넣어둠
-
-        itemRepository.save(item);
-
-        return "validation/v2/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-        return "validation/v2/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV5(Item item) {
-        itemRepository.save(item);
-        return "redirect:/validation/v2/items/" + item.getId();
-    }
-
     @PostMapping("/add")
-    public String addItemV6(Item item, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV1(Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
 
-        // 검증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();
-
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
-            errors.put("itemName", "상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError(
+                    "item", "itemName", "상품 이름은 필수 입니다."
+            ));
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            errors.put("price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다.");
+            bindingResult.addError(new FieldError(
+                    "item", "price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다."
+            ));
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-            errors.put("quantity", "수량은 최대 9,999개 까지 허용합니다.");
+            bindingResult.addError(new FieldError(
+                    "item", "quantity", "수량은 최대 9,999개 까지 허용합니다."
+            ));
         }
 
         // 특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
         // 검증에 실패하면 다시 입력 폼으로
-        if (!errors.isEmpty()) {
-            log.info("errors={}", errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult={}", bindingResult);
 
             return "validation/v2/addForm";
         }
